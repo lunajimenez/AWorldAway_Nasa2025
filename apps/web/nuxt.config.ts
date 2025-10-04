1b66178e42c7f8d1d4070c18ed39db5ed185ff85
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-10-04 10:08:33 -0500

    feat: configuring shadcn-vue and adding some more modules
    BREAKING CHANGE: shadcn-vue has a bug which tells something like 'Overriding component <name>...' so the workaround was to modify the registry in package.json

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,50 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image']
-})
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    css: ["~/assets/css/tailwind.css"],
+    vite: {
+        plugins: [tailwindcss()],
+        server: {
+            fs: {
+                strict: true,
+            },
+        },
+        build: {
+            sourcemap: false,
+        },
+    },
+    imports: {
+        dirs: ["composables/**", "./shared/utils/**"],
+        imports: [{ from: "vue-sonner", name: "toast" }],
+    },
+    nitro: {
+        imports: {
+            dirs: ["./shared/utils/**"],
+        },
+    },
+    modules: [
+        "@nuxt/eslint",
+        "@nuxt/image",
+        "shadcn-nuxt",
+        "@nuxtjs/color-mode",
+        "@vueuse/nuxt",
+        "@vee-validate/nuxt",
+    ],
+    shadcn: {
+        prefix: "",
+        componentDir: "~/components/ui",
+    },
+    colorMode: {
+        classSuffix: "",
+    },
+    eslint: {
+        config: {
+            standalone: false,
+        },
+    },
+    veeValidate: {
+        autoImports: false,
+    },
+});
